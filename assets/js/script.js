121f4d98613badd01c9a2b0c4eca04f993b36882
Sittichai Chaikamol <nu@mcp.local>
2020-02-20 17:00:52 -0600

    color

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,10 @@
-
-
 $( document ).ready(function() {
-    $(".lead").text(moment().format("dddd, MMMM Do YYYY, h:mm A"));
+    $(".date").text(moment().format("dddd, MMMM Do YYYY, h:mm A"));
     for (var i = 9; i < 22; i++ ) {
         var row =$(`<div data-time=${i} id="${i}" class="row">`);
         var time =$('<div class="col-md-1"><p class=" time">' + timing(i) + '<p/>');
-        var text = $(`<div class="col-md-9"><textarea class="text" id=antothertext"${i} class="input" placeholder="Add Event"></textarea>`);
-        var save =$(`<div class="col-md-2" id"btn"><button class="save-btn" id=${i}><i class="fa fa-save"></i></button>`);
+        var text = $(`<div class="col-md-9"><textarea class="text" id=input${i} class="event" placeholder="Add Event"></textarea>`);
+        var save =$(`<div class="col-md-2"><button class="save-btn" id=${i}><i class="fa fa-save"></i></button>`);
         console.log(save)
 
         row.append(time);
@@ -14,7 +12,7 @@ $( document ).ready(function() {
         row.append(save);
         console.log(row)
 
-        $("#scheduler").append(row)
+        $("#content").append(row)
        
         getLocalStorage(i);
     }
@@ -24,6 +22,7 @@ $( document ).ready(function() {
             $(`${x}`).text(value);
         }
     }
+   
     
     function timing(hours) {
         var hour = hours >= 12 ? 'PM' : 'AM';
@@ -31,7 +30,24 @@ $( document ).ready(function() {
         hours = hours ? hours : 12;
         return hours + hour;
     }
-  
-    time.setAttribute("class", "time");
-    text.setAttribute("class", "text");
+   timing();
+   
+   function timeColor() {
+    var currentTime = new Date().getHours();
+    for (var i = 9; i < 22; i++) { 
+    console.log(currentTime, $(`#${i}`).data("time"));
+     if ($(`#${i}`).data("time") == currentTime){
+        $(`#input${i}`).addClass( "present");
+    } else if (currentTime < $(`#${i}`).data("time")) {
+        $(`#input${i}`).addClass( "future");
+            }
+        }
+    }
+   setInterval(function(){
+    timeColor();
+   },);
+   
+
     });
+
+
